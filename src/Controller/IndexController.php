0293ec4eec5d6d74541cf6a9898d877a3ed533f7
Adrian Tracz <xgutek@gmail.com>
2023-01-17 15:16:25 +0100

    Blog index responsive bootstrap template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,20 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): JsonResponse
+    public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/IndexController.php',
-        ]);
+        return $this->render('index.html.twig');
     }
-
-    
-
 }
